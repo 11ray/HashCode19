f05219371cd3308a4aa639519b5d679033a1059f
Javier Rodríguez <javierrd96@gmail.com>
2019-02-28 20:26:53 +0100

    ww

--- mock-solver.py
+++ mock-solver.py
@@ -1,42 +1,14 @@
-
-
-def slideComb(album,Vlist):
-
-    matSlides = []
-    sList = []
-    for i in range(N):
-        slist.append(0)
-        matSlides.append[]
-        for j in range(N):
-            matSlides[i].append(-1)
-        
-
-    for key,horizontal in album["H"]:
-        slist[key]=key
-    
-    for i in range(len(Vlist)):
-        slist[Vlist[i][0]] = Vlist[i]
-
-    for i in range(N):
-        for j in range(N):
-            if i != j:
-                matSlides(i,j) = generateValue(album, slist(i),sList(j))
-
-    return matSlides, sList
-
-
-
 def generateValue( album, slide1, slide2):
 
     if type(slide1) is int: 
         list1 = album["H"][slide1]
-    else
-        list1 = list(set(album["V"][slide1][0] + album["V"][slide1][1]))
+    else:
+        list1 = list(set(album["V"][slide1[0]] + album["V"][slide1[1]]))
 
     if type(slide2) is int: 
         list2 = album["H"][slide2]
-    else
-        list2 = list(set(album["V"][slide2][0] + album["V"][slide2][1]))
+    else:
+        list2 = list(set(album["V"][slide2[0]] + album["V"][slide2[1]]))
     
     intersection = len(list(set(list1) & set(list2)))
     diff1 = len(list(set(list1) - set(list2)))
@@ -44,6 +16,38 @@ def generateValue( album, slide1, slide2):
 
     return min(intersection,diff1,diff2)
 
+def slideComb(album,Vlist):
 
+    matSlides = []
+    sList = []
+    for i in range(N):
+        sList.append(0)
+        matSlides.append([])
+        for j in range(N):
+            matSlides[i].append(-1)
+        
+
+    for key in album["H"]:
+        sList[key]=key
+    
+    for i in range(len(Vlist)):
+        sList[Vlist[i][0]] = Vlist[i]
+
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                matSlides[i][j] = generateValue(album, sList[i],sList[j])
+
+    return matSlides, sList
+
+
+
+N=4
+album={'H': {0: ['cat', 'beach', 'sun'], 3: ['garden', 'cat']}, 'V': {1: ['selfie', 'smile'], 2: ['garden', 'selfie']}}
+Vlist=[[1,2]]
+
+matSlides, sList = slideComb(album,Vlist)
+
+print(matSlides)
 
 
